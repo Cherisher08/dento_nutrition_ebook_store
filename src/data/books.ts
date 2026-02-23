d91c81e9a2d0df66d163115abfac19d616bb3066
Cherisher08 <vigu2002ram@gmail.com>
2026-02-23 22:44:05 +0530

    update path to remove public in the image path

--- src/data/books.ts
+++ src/data/books.ts
@@ -53,7 +53,7 @@ export const books: Book[] = [
       "Child-friendly yet adult-approved",
     ],
     coverColor: "bg-orange-500",
-    cover_image: "/public/100_nutritious_recipes.png",
+    cover_image: "/100_nutritious_recipes.png",
     authorProfile: {
       about:
         "Dento Nutrition is dedicated to providing scientifically backed, nutritionally balanced, and delicious recipes for families. With a focus on child nutrition and healthy eating habits, each recipe is crafted to ensure it meets the highest standards of taste and health.",
@@ -88,7 +88,7 @@ export const books: Book[] = [
       "Helps improve weight gradually",
     ],
     coverColor: "bg-blue-500",
-    cover_image: "/public/weight_gain_recipes_1year_book_cover.png",
+    cover_image: "/weight_gain_recipes_1year_book_cover.png",
     authorProfile: {
       about:
         "Dento Nutrition is dedicated to providing scientifically backed, nutritionally balanced, and delicious recipes for families. With a focus on child nutrition and healthy eating habits, each recipe is crafted to ensure it meets the highest standards of taste and health.",
@@ -123,7 +123,7 @@ export const books: Book[] = [
       "Nutrient-rich for infants",
     ],
     coverColor: "bg-green-500",
-    cover_image: "/public/weight_gain_recipes_612mons_book_cover.png",
+    cover_image: "/weight_gain_recipes_612mons_book_cover.png",
     authorProfile: {
       about:
         "Dento Nutrition is dedicated to providing scientifically backed, nutritionally balanced, and delicious recipes for families. With a focus on child nutrition and healthy eating habits, each recipe is crafted to ensure it meets the highest standards of taste and health.",
